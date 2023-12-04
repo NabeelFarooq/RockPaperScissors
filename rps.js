65152f18ab63e5c1142f5e70cd828c5aae0b6a4b
Nabeel <42202507+NabeelFarooq@users.noreply.github.com>
2023-12-04 16:23:34 +0530

    Complete UI of ROCK PAPER SCISSORS!

--- rps.js
+++ rps.js
@@ -1,4 +1,8 @@
-
+let userPoints = 0;
+let compPoints = 0;
+const buttons = document.querySelectorAll("button");
+const result = document.querySelector(".result");
+result.setAttribute('style', 'white-space: pre;');
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
     //console.log(num);
@@ -12,102 +16,88 @@ function getComputerChoice() {
         return "scissors";
     }
 }
-function winMessage(){
-    const result = document.querySelector(".result");
-    result.textContent = "You win!";
-    ++uWin;
-}
-function loseMessage(){
-    const result = document.querySelector(".result");
-    result.textContent = "You lose!";
-    ++compWin;
-}
-function drawMessage(){
-    const result = document.querySelector(".result");
-    result.textContent = "Draw!";
-}
-function displayFinalResult(){
-    const finalResult = document.querySelector("#finalResult");
-    if(compWin === uWin){
-        finalResult.textContent = "Final Result: Draw!";
-    } else if(compWin > uWin){
-        finalResult.textContent = "Final Result: You lose!";
-    } else{
-        finalResult.textContent = "Final Result: You win!";
-    }
-
+function displayRunningScore() {
+    const yourScore = document.querySelector("div.points > span#you");
+    const computerScore = document.querySelector("div.points > span#computer");
+    yourScore.textContent =  userPoints;
+    computerScore.textContent =  compPoints;
 }
 function playRound(playerSelection, computerSelection) {
+
+    if (userPoints === 5 || compPoints === 5) {
+        let winner = '';
+        if (userPoints > compPoints) {
+            winner = 'You';
+        } else {
+            winner = 'Computer';
+        }
+        
+        result.textContent += "\r\n" + winner + " won the game.To play again, please refresh the page."
+        disableButtons();
+        return;
+    }
+
     if (playerSelection === 'rock') {
         if (computerSelection === 'rock') {
-            drawMessage();
+            displayRunningScore()
+            result.textContent += "Draw\r\n";
         }
         if (computerSelection === 'paper') {
-            loseMessage();
+            displayRunningScore()
+            ++compPoints;
+            result.textContent += "You lost this round\r\n";
         }
         if (computerSelection === 'scissors') {
-            winMessage();
+            displayRunningScore()
+            ++userPoints;
+            result.textContent += "You won this round\r\n";
         }
     }
+
     if (playerSelection === 'paper') {
         if (computerSelection === 'paper') {
-            drawMessage();
+            displayRunningScore()
+            result.textContent += "Draw\r\n";
         }
         if (computerSelection === 'scissors') {
-            loseMessage();
+            displayRunningScore()
+            ++compPoints;
+            result.textContent += "You lost this round\r\n";
         }
         if (computerSelection === 'rock') {
-            winMessage();
+            displayRunningScore()
+            ++userPoints;
+            result.textContent += "You won this round\r\n";
         }
     }
+
     if (playerSelection === 'scissors') {
         if (computerSelection === 'paper') {
-            winMessage();
+            displayRunningScore()
+            ++userPoints;
+            result.textContent += "You won this round\r\n";
         }
         if (computerSelection === 'scissors') {
-            drawMessage();
+            displayRunningScore()
+            result.textContent += "Draw\r\n";
         }
         if (computerSelection === 'rock') {
-            loseMessage();
+            displayRunningScore()
+            ++compPoints;
+            result.textContent += "You lost this round\r\n";
         }
     }
 }
-function game() {
-    numberOfMouseClicks = 0;
-    compWin = 0;
-    uWin = 0;
-    const rock = document.querySelector("#rock");
-    const paper = document.querySelector("#paper");
-    const scissor = document.querySelector("#scissors");
-    const yourChoice = document.querySelector("#yChoice");
-    const computerChoice = document.querySelector("#cChoice");
-    if(numberOfMouseClicks<5){
-        rock.addEventListener('click', () => {
-            ++numberOfMouseClicks;
-            yourChoice.textContent = "Your choice: Rock";
-            const computerSelection = getComputerChoice();
-            computerChoice.textContent = `Computer choice: ${computerSelection}`;
-            playRound("rock", computerSelection);
-        });
-        paper.addEventListener('click', () => {
-            ++numberOfMouseClicks;
-            yourChoice.textContent = "Your choice: Paper";
-            const computerSelection = getComputerChoice();
-            computerChoice.textContent = `Computer choice: ${computerSelection}`;
-            playRound("paper", computerSelection);
-        });
-        scissor.addEventListener('click', () => {
-            ++numberOfMouseClicks;
-            yourChoice.textContent = "Your choice: Scissors";
-            const computerSelection = getComputerChoice();
-            computerChoice.textContent = `Computer choice: ${computerSelection}`;
-            playRound("scissors", computerSelection);
-        });
-    }
-    else{
-        displayFinalResult();
-    }
-    
+// function game() {
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        playRound(button.id, getComputerChoice());
+    })
+});
+// }
+function disableButtons() {
+    buttons.forEach(button => {
+        button.disabled = true
+    })
 }
-
-game();
+// game();
